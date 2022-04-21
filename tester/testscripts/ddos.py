ce72a482cadba290e3c9f716420b7de8c27f1534
Robin Adelwarth <radelwar@1-E-15.42heilbronn.de>
2022-04-21 20:26:06 +0200

    save

--- tester/testscripts/ddos.py
+++ tester/testscripts/ddos.py
@@ -1,17 +1,22 @@
+import traceback
 import requests
 import threading
 import time
 
 def thread_get_request(name):
 	i = 0
-	for i in range (15):
-		r = requests.get('http://localhost:1000/')
-		print(r.status_code)
-		print(r.text);
+	for i in range (2000):
+		try:
+			r = requests.get('http://localhost:1000/')
+			print(r.status_code)
+			print(r.text)
+		except Exception:
+			traceback.print_exc()
+			exit()
 
 if __name__ == "__main__":
 	threads = list()
-	for index in range(5):
+	for index in range(50):
 		x = threading.Thread(target=thread_get_request, args=(index,))
 		threads.append(x)
 		x.start()
