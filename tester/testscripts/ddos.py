0ef951930c24479995cf69df536bb5c8841fbc65
Felix Haring <fharing@1-E-13.42heilbronn.de>
2022-04-21 09:51:19 +0200

    added python script

---
+++ tester/testscripts/ddos.py
@@ -0,0 +1,20 @@
+import requests
+import threading
+import time
+
+def thread_get_request(name):
+	i = 0
+	for i in range (15):
+		r = requests.get('http://localhost:1000/')
+		print(r.status_code)
+		print(r.text);
+
+if __name__ == "__main__":
+	threads = list()
+	for index in range(5):
+		x = threading.Thread(target=thread_get_request, args=(index,))
+		threads.append(x)
+		x.start()
+
+	for index, thread in enumerate(threads):
+		thread.join()
